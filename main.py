037225efbe79f359515236ff6fc9dfb8cc08b359
Nayma-konok <naymakonok002@gmail.com>
2025-10-21 22:01:55 +0100

    Added Gradio UI for Nova chatbot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import gradio as gr
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser   
@@ -31,14 +32,45 @@ prompt=ChatPromptTemplate.from_messages([
 
 chain= prompt | llm | StrOutputParser()
 
-history=[]
+def Chat(user_input,history):
+    langchain_history=[]
+    for item in history:
+        if item["role"]=="user":
+            langchain_history.append(HumanMessage(content=item["content"]))
+        elif item["role"]=="assistant":
+            langchain_history.append(AIMessage(content=item["content"]))
+    response=chain.invoke({"input":user_input,"history":langchain_history})
 
-while True:
-    user_input=input("You: ")
-    if user_input=="exit":
-        break
-    #history.append({"role":"user","content":user_input})
-    response=chain.invoke({"input":user_input,"history":history})
-    print(f"Nova: {response}")
-    history.append(HumanMessage(content=user_input))
-    history.append(AIMessage(content=response))
+    return "", history+[{"role":"user",'content':user_input},
+                        {"role":"assistant",'content':response}]
+
+
+def clear_chat():
+    return "",[]
+
+
+page=gr.Blocks(
+    title="SuperNova",
+    theme=gr.themes.Soft(),
+)
+
+with page:
+    gr.Markdown(
+        """
+        # Chat With Nova
+        Welcome to your personal conversation with Nova
+        """
+)
+    chatbot=gr.Chatbot(type="messages",
+                       avatar_images=[None,"supernova_1993J.jpg"],
+                       show_label=False)
+
+    msg=gr.Textbox(show_label=False,
+                   placeholder="Ask Nova anything...")
+
+    msg.submit(Chat, [msg, chatbot], [msg, chatbot])
+
+    Clear=gr.Button("Clear Chat",variant="secondary")
+    Clear.click(clear_chat,outputs=[msg, chatbot])
+
+page.launch(share=True)
